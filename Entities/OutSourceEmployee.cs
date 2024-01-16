da14d9d467cf3ca651503db9798b2f84effe2697
Vinicius Morais <vinidev.morais@gmail.com>
2024-01-16 11:57:50 -0300

    created class OutSourceEmployee

---
+++ Entities/OutSourceEmployee.cs
@@ -0,0 +1,20 @@
+namespace Worker.Entities
+{
+    public class OutSourceEmployee : Employee
+    {
+        public double AdditionalCharge { get; set; }
+
+        public OutSourceEmployee()
+        {
+
+        }
+        public OutSourceEmployee(string name, int hours, double valuePerHour, double additionalCharge) : base(name, hours, valuePerHour)
+        {
+            AdditionalCharge = additionalCharge;
+        }
+        public override double Payment()
+        {
+            return base.Payment() + 1.1 * AdditionalCharge;  
+        }
+    }
+}
